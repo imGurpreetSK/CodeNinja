b2022eb29f40a0c5baa4183669cbcae99bc814b2
Gurpreet <gurpreetsk@hotmail.com>
2016-10-20 09:23:23 +0530

    - changes to code, working on semantic analysis of answers

--- codeNinja.py
+++ codeNinja.py
@@ -12,37 +12,27 @@ at_bot = "<@" + str(botid) + ">:"
 client_slack = SlackClient(config.slack_token['SLACK_TOKEN'])
 
 
-def bot_id():
-    api_call = client_slack.api_call("users.list")
-    if api_call.get('ok'):
-        members = api_call.get('members')
-        for user in members:
-            if 'name' in user and user.get('name') == botname:
-                print(user.get('id'))
-                return user.get('id')
-
-
 def parse_data(slack_data):
-    input_data = slack_data
-    if input_data and len(input_data) > 0:
-        for data in input_data:
-            if data and 'text' in data and data['user'] != bot_id():
+    inputdata = slack_data
+    if inputdata and len(inputdata) > 0:
+        for data in inputdata:
+            if data and 'text' in data and data['user'] != botid:
                 return data['text'], data['channel']
     return None, None
 
 
 def chat(input, channel):
     input = input.replace(at_bot + " ", "")
-    SOlink = "http://stackoverflow.com"
+    SO_url = "http://stackoverflow.com"
     for url in search(urllib.quote_plus(input.encode('utf8'))):
         if "http://stackoverflow.com/" in url:
-            SOlink = url
+            SO_url = url
             client_slack.api_call("chat.postMessage", channel=channel, text=str(url), as_user=True)
             break
         else:
             continue
     try:
-        page = urllib2.urlopen(SOlink)
+        page = urllib2.urlopen(SO_url)
         soup = BeautifulSoup(page.read())
         result = soup.find(attrs={'class': 'answer accepted-answer'})
         res = result.find(attrs={'class': 'post-text'})
@@ -51,17 +41,17 @@ def chat(input, channel):
                 a.string = ' '
         client_slack.api_call("chat.postMessage", channel=channel, text="```" + res.get_text() + "```", as_user=True)
     except IndexError:
-        page = urllib2.urlopen(SOlink)
+        page = urllib2.urlopen(SO_url)
         soup = BeautifulSoup(page.read())
         result = soup.find(attrs={'class': 'answer'})
         res = result.find(attrs={'class': 'post-text'})
         for a in res:
-            if (a.string is None):
+            if a.string is None:
                 a.string = ' '
         client_slack.api_call("chat.postMessage", channel=channel, text="```" + res.get_text() + "```", as_user=True)
     except:
         print("Could not parse")
-        client_slack.api_call("chat.postMessage", channel=channel, text="Could not find a relevant link", as_user=True)
+        # client_slack.api_call("chat.postMessage", channel=channel, text="Could not find a relevant link", as_user=True)
         raise
 
 
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Slack bot token and id
 
-slack_token = dict(SLACK_TOKEN = ' <Your-id-here> ')
-bot_id = dict(BOT_ID = ' <your-id-here> ')
+slack_token = dict(SLACK_TOKEN = 'xoxb-92151848725-IHW54eBWCOVIuoFchAXGJg7S') # <Your-id-here>
+bot_id = dict(BOT_ID = 'U2Q4FQYMB')  # <your-id-here>
