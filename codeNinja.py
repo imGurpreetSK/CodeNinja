a315668c12704c8a4f34cdda2ab87dd2a99a810e
Gurpreet <gurpreetsk@hotmail.com>
2016-10-20 08:13:58 +0530

    - changes to code, working on semantic analysis of answers

--- codeNinja.py
+++ codeNinja.py
@@ -4,7 +4,6 @@ import urllib2
 from bs4 import BeautifulSoup
 from google import search
 from slackclient import SlackClient
-
 import config
 
 botname = 'codeninja'
@@ -24,43 +23,42 @@ def bot_id():
 
 
 def parse_data(slack_data):
-    inputdata = slack_data
-    if inputdata and len(inputdata) > 0:
-        for data in inputdata:
+    input_data = slack_data
+    if input_data and len(input_data) > 0:
+        for data in input_data:
             if data and 'text' in data and data['user'] != bot_id():
                 return data['text'], data['channel']
     return None, None
 
 
-def chat(inputcmd, channel):
-    # botid = "<@" + str(bot_id()) + ">:"
-    inputcmd = inputcmd.replace("<@U2Q4FQYMB> ","")
-    soverflowurl = "http://stackoverflow.com"
-    for url in search(urllib.quote_plus(inputcmd.encode('utf8'))):
+def chat(input, channel):
+    input = input.replace(at_bot + " ", "")
+    SOlink = "http://stackoverflow.com"
+    for url in search(urllib.quote_plus(input.encode('utf8'))):
         if "http://stackoverflow.com/" in url:
-            soverflowurl = url
+            SOlink = url
             client_slack.api_call("chat.postMessage", channel=channel, text=str(url), as_user=True)
             break
         else:
             continue
     try:
-        page = urllib2.urlopen(soverflowurl)
+        page = urllib2.urlopen(SOlink)
         soup = BeautifulSoup(page.read())
         result = soup.find(attrs={'class': 'answer accepted-answer'})
         res = result.find(attrs={'class': 'post-text'})
         for a in res:
-            if (a.string is None):
+            if a.string is None:
                 a.string = ' '
-        client_slack.api_call("chat.postMessage", channel=channel, text="```"+res.get_text()+"```", as_user=True)
+        client_slack.api_call("chat.postMessage", channel=channel, text="```" + res.get_text() + "```", as_user=True)
     except IndexError:
-        page = urllib2.urlopen(soverflowurl)
+        page = urllib2.urlopen(SOlink)
         soup = BeautifulSoup(page.read())
         result = soup.find(attrs={'class': 'answer'})
         res = result.find(attrs={'class': 'post-text'})
         for a in res:
             if (a.string is None):
                 a.string = ' '
-        client_slack.api_call("chat.postMessage", channel=channel, text= "```"+res.get_text()+"```", as_user=True)
+        client_slack.api_call("chat.postMessage", channel=channel, text="```" + res.get_text() + "```", as_user=True)
     except:
         print("Could not parse")
         client_slack.api_call("chat.postMessage", channel=channel, text="Could not find a relevant link", as_user=True)
@@ -71,9 +69,9 @@ def ninjafy():
     if client_slack.rtm_connect():
         print("Connected")
         while True:
-            inputcmd, channel = parse_data(client_slack.rtm_read())
-            if inputcmd and channel:
-                chat(inputcmd, channel)
+            input, channel = parse_data(client_slack.rtm_read())
+            if input and channel:
+                chat(input, channel)
             time.sleep(1)
     else:
         print("Connection failed")
@@ -81,4 +79,3 @@ def ninjafy():
 
 if __name__ == '__main__':
     ninjafy()
-
